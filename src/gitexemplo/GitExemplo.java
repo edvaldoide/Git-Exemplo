8bf687a4ec922ee55a95b48dec89cb0f6909dc3c
Edvaldo Ide <edvaldoide@gmail.com>
2025-03-13 12:46:36 -0300

    Teste para desfazer commit

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -4,6 +4,8 @@ public class GitExemplo {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        System.out.println("Teste para desfazer alterações");
     }
 
 }
